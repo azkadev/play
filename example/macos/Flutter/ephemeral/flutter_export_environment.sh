482442438b013dcd6282d55fd2a0fe8ab3b52642
￴ <>
2022-11-30 09:24:12 +0700

    update

--- example/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/hexaminate/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/hexaminate/Downloads/play/example"
+export "FLUTTER_APPLICATION_PATH=/home/hexaminate/Downloads/Programs/play/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=1"
+export "FLUTTER_BUILD_NUMBER=2"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
